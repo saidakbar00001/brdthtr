1c1b593d40ce8f2a102e129a96ffeb556b34c6a6
saidakbar00001 <ilyasxonvsaidakbar@gmail.com>
2025-08-20 22:42:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let ism=prompt("Ism kiriting")
+let familangiz=prompt("Familiya kiriting")
+let sa=prompt("Yoshingizni kiriting")
+let Tugulgan=prompt("Tug'lgan yilingizni kiriting")
+let sal=prompt("Yashash tumangizni kiriting")
+let tel=prompt("Qayarda oqishingizni kiriting")
+let sdv=prompt("Sizning sevimli dasturlash tilingizni kiriting")
+ alert("Malimot uchun raxmat")
+console.log("Sizning ism: " + ism);
+console.log("Sizning familiya: " + familangiz);
+console.log("Sizning yosh: " + sa);
+console.log("Sizning tug'ilgan yil: " + Tugulgan);
+console.log("Sizning yashash tumani: " + sal);
+console.log("Sizning o'qish joyi: " + tel);
+console.log("Sizning sevimli dasturlash tili: " + sdv);
